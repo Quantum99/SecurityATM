87f1b5b0b16e6f10a45dd4fd2a48a6eae162d33c
Quantum99 <miketech66@outlook.com>
2016-11-20 21:38:41 +0000

    Implementing the AES

--- ATMServer/src/AES.java
+++ ATMServer/src/AES.java
@@ -64,4 +64,17 @@ public class AES {
         }
         return null;
     }
+    
+  /*  public static void main(String[] args) 
+    {
+        final String secretKey = "ssshhhhhhhhhhh!!!!";
+         
+        String originalString = "howtodoinjava.com";
+        String encryptedString = AES.encrypt(originalString, secretKey) ;
+        String decryptedString = AES.decrypt(encryptedString, secretKey) ;
+         
+        System.out.println(originalString);
+        System.out.println(encryptedString);
+        System.out.println(decryptedString);
+    }*/
 }
